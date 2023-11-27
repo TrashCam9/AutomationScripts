e139ae1d6d6d72a35fcb19ec87b319944c5166be
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2023-11-27 13:49:22 -0500

    Add New-VirtualPortGroupWithVS function to module

---
+++ VMWare/Manage-Networks/Manage-Networks.psm1
@@ -0,0 +1,16 @@
+function New-VirtualPortGroupWithVS {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $VirtualSwitchName,
+        [Parameter(Mandatory=$true)]
+        [string] $VMHost,
+        [Parameter(Mandatory=$true)]
+        [Int32] $NumPorts,
+        [Parameter(Mandatory=$true)]
+        [Int32] $Mtu,
+        [Parameter(Mandatory=$true)]
+        [string] $PortGroupName
+    )
+    $vs = New-VirtualSwitch -Name $VirtualSwitchName -VMHost $VMHost -NumPorts $NumPorts -Mtu $Mtu
+    New-VirtualPortGroup -VirtualSwitch $vs -Name $PortGroupName
+}j
